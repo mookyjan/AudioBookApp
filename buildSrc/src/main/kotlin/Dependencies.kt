7543940d152a6327a6cfac0bae0da2456ce9ee62
Mudassir Khan <mudassir45khan@yahoo.com>
2020-08-16 16:28:46 +0800

    feat:update the gradle dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,6 +2,7 @@ object App{
     const val compileSdk = 29
     const val minSdk = 21
     const val targetSdk = 29
+    const val buildToolVersion ="29.0.3"
     const val versionCode = 1
     const val versionName = "1.0.0"
 }
