c6c8631fb7d6c9245ea9c885899fb24ed3d84f5c
Mudassir Khan <mudassir45khan@yahoo.com>
2020-08-21 10:57:05 +0800

    test:writing more test cases for audiobook useCase

--- domain/src/main/java/com/mudassir/domain/UseCase.kt
+++ domain/src/main/java/com/mudassir/domain/UseCase.kt
@@ -4,7 +4,7 @@ import io.reactivex.Single
 
 abstract class UseCase<in Params,T> protected constructor(private val schedulers:SchedulerProvider) {
 
-    protected abstract fun buildUseCaseObservable(params: Params): Single<T>
+     abstract fun buildUseCaseObservable(params: Params): Single<T>
 
     fun execute(params: Params) : Single<T> {
         val observable = this.buildUseCaseObservable(params)
--- domain/src/test/java/com/mudassir/domain/usecase/GetAudioBookListUseCaseTest.kt
+++ domain/src/test/java/com/mudassir/domain/usecase/GetAudioBookListUseCaseTest.kt
@@ -1,13 +1,13 @@
 package com.mudassir.domain.usecase
 
 import com.mudassir.domain.SchedulerProvider
-import com.mudassir.domain.entity.AudioBookEntity
 import com.mudassir.domain.gateway.AudioBookGateWay
 import com.mudassir.domain.utils.TestSchedulers
 import com.mudassir.domain.utils.audioBookEntity
 import com.nhaarman.mockito_kotlin.eq
 import com.nhaarman.mockito_kotlin.mock
 import io.reactivex.Single
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -30,19 +30,40 @@ class GetAudioBookListUseCaseTest{
 
     @Test
     fun `get list of audio book`(){
-
         //Given
         val mockList = audioBookEntity
-
-        //when
         Mockito.`when`(mockAudioBookListGateWay.getAudioBookList(false)).thenReturn(eq(Single.just(mockList)))
-
+        //when
         val testObserver =getAudioBookListUseCase.execute(false).test()
-
         //then
         testObserver.assertComplete()
             .assertNoErrors()
             .assertValue(mockList)
+    }
 
+    @Test
+    fun `given and expected list should be matched`(){
+        //Given
+        val expected = Single.just(audioBookEntity)
+        Mockito.`when`(mockAudioBookListGateWay.getAudioBookList()).thenReturn(expected)
+        //when
+        val result = getAudioBookListUseCase.buildUseCaseObservable(false)
+        //then
+        assertEquals(expected,result)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `should throw exception on error`(){
+        //Given
+        val error = Throwable()
+        Mockito.`when`(mockAudioBookListGateWay.getAudioBookList()).thenReturn(Single.error(error))
+        //when
+        val result = getAudioBookListUseCase.execute(false).test()
+
+        //should
+        result.assertNotComplete()
+            .assertError(error)
+            .assertNoValues()
     }
 }
